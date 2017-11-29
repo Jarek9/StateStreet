b4f63b7da14acdc3057b5500d90976341d043770
Jarek9 <jbabecki@wp.pl>
2017-11-29 22:49:44 +0100

    Update Main.java

--- StateStreet/src/Main.java
+++ StateStreet/src/Main.java
@@ -2,7 +2,7 @@ import java.io.File;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 				
 		File path1 = new File("res/data/gmbh");
